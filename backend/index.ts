79e43ec8ba1563a5128cf194d5bc87314573bed5
ashrafalam123 <108684087+ashrafalam123@users.noreply.github.com>
2025-08-25 15:39:10 +0530

    updated headers for event-stream

--- backend/index.ts
+++ backend/index.ts
@@ -24,7 +24,9 @@ app.post('/chat', async (req, res) => {
         content : data!.message
     })
 
-    res.setHeader('Content-Type', 'text/event-stream; charset=utf-8');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Connection', 'keep-alive');
 
     let message = ""
